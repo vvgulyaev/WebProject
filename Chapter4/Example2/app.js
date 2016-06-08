337d0e4a5345f7d3c1d81dce49cba6fba0242ced
Vyacheslav Gulyaev <vv_gulyaev@mail.ru>
2016-06-08 20:09:35 +0300

    modified comment appending

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,7 +1,12 @@
-var func = function () {
-							"use strict";
-							window.alert("hello, world!");
-						};
-
-$(document).ready(func);
-
+var main = function () 
+{
+	"use strict";
+	$(".comment-input button").on("click", function (event) 
+	{
+		var $new_comment = $("<p>"),
+		comment_text = $(".comment-input input").val();
+		$new_comment.text(comment_text);
+		$(".comments").append($new_comment);
+	});
+};
+$(document).ready(main);
